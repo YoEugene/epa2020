005eadcdd9246c6e9afb352d3226307ae6b0d74e
root <root@training-temp.c.eighth-feat-281709.internal>
2020-06-29 08:18:12 +0000

    [Change] modify train file

--- train.py
+++ train.py
@@ -6,7 +6,10 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', help='stations')
 parser.add_argument('-pos', help='position')
+parser.add_argument('-verbose', help='verbose')
 args = parser.parse_args()
+
+verbose = args.verbose if args.verbose else 0
 
 # Import relevant items
 import pandas as pd
@@ -25,7 +28,7 @@ finished_stations = []
 for position in [args.pos]:
     print('===========' + position + '===========')
     # for station in reversed(os.listdir('/'.join([data_folder_prefix, position]))):
-    for station in args.s.split('    '):
+    for station in args.s.split('  '):
         if station in finished_stations: continue
         print('Start training on: ' + station)
         # try:
@@ -39,7 +42,7 @@ for position in [args.pos]:
             df_test = pd.read_csv('/'.join([data_folder_prefix, position, station, str(hour)]) + '/gbdt_2019_nearby.csv')
             X_test, y_test = df_test.drop(['PM2.5_TARGET','TIME'], axis=1), df_test['PM2.5_TARGET']
 
-            reg = GradientBoostingRegressor(verbose=1, random_state=42)
+            reg = GradientBoostingRegressor(verbose=verbose, random_state=42)
             reg.fit(X_train, y_train)
 
             with open('/'.join([data_folder_prefix, position, station, str(hour), 'model.pickle']), 'wb') as f:
@@ -47,7 +50,7 @@ for position in [args.pos]:
                 print('hour ' + str(hour) + ' saved.')
 
             MAE_list.append(str(sum(abs(reg.predict(X_test) - y_test))/len(X_test)))
-            print(MAE_list[-1])
+            # print(MAE_list[-1])
         print(','.join(MAE_list))
         # except Exception as e:
         #     print(repr(e))
