0fead5ff97ac873fa017206919f31bd60aba96ff
Eugene Hsu <eugenehsu@livapp.com>
2020-07-07 22:31:03 +0800

    [Bug] Minor typo fix

--- utils.py
+++ utils.py
@@ -13,11 +13,11 @@ def read_config():
 
 
 def csv_to_parquet(str_csv_path_filename):
-    df=pd.read_csv(str_csv_path_filename)
-    table=pa.Table.from_pandas(df)
-    pq.write_table(table,f"{str_csv_path_filename[:-4]}.parquet",compression="gzip")    
+    df = pd.read_csv(str_csv_path_filename)
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table,"{}.parquet".format(str_csv_path_filename[:-4]), compression="gzip")
 
 
 def parquet_to_csv(str_parquet_path_filename):
-    df=pd.read_parquet(str_parquet_path_filename,engine="pyarrow")
-    df.to_csv(f"{str_parquet_path_filename[:-8]}.csv",index=False)
+    df = pd.read_parquet(str_parquet_path_filename, engine="pyarrow")
+    df.to_csv("{}.csv".format(str_parquet_path_filename[:-8]), index=False)
