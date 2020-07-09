ea9130d24a0455127b1fb4bc45632c6d01103fba
Eugene Hsu <eugenehsu@livapp.com>
2020-07-09 23:57:34 +0800

    [Change] modify pipeline

--- pipeline.py
+++ pipeline.py
@@ -23,11 +23,11 @@ def main(cfg):
     # # Step 2
     # move_csv_to_area.main(cfg)
 
-    # # Step 3
-    # LSTM_data_preprocess.main(cfg)
+    # Step 3
+    LSTM_data_preprocess.main(cfg)
 
     # Step 4 - 1
-    # GBDT_data_preprocess.main(cfg)
+    GBDT_data_preprocess.main(cfg)
 
     # Step 4 - 2
     GBDT_add_nearby_data.main(cfg)
